7b3ea98c7efd23b2d7a437eac11b5e24229393a9
Maciej <macko939@gmail.com>
2021-01-24 17:02:36 +0000

    Update reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -7,21 +7,25 @@ class Solution(object):
         negative = False
         result = 0
         
+        # Detect if the integer is negative. If yes, convert it to positive and store the information for later.
         if x < 0:
             x = x * -1
             negative = True
             
         
-        for i in range(len(str(x))):
+        # Pop the least significant digit of the x and truncate x accordingly
+        for i in range(len(str(x))): 
             pop = x % 10
             x = x/10
-        
+            
+            # Concatanate already obtained result with the most recently popped number
             result = int(str(result)+str(pop))
-                    
+        
+        # Detect if the resulting number is exceeding 32bits, return 0 if yes
         if((result < (-2**31)) or (result > (2**31))):
             return 0 
            
-           
+        # Turn the result negative if the original value was also negative
         if negative:
             result = result * -1
             
