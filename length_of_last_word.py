1189a3c533aa4b7968ca9f6efad6ba9aa7663016
Maciej <macko939@gmail.com>
2021-01-30 16:37:15 +0000

    Update length_of_last_word.py

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -4,15 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # Split the string s into an array of words
         wordList = s.split()
-        for i in range(len(wordList)):
-            wordList[i] = wordList[i].strip()
+        for i in range(len(wordList)): # Iterate through all words in the array
+            wordList[i] = wordList[i].strip() # Remove whitespace from the words to prevent it from being counted and to catch empty s inputs
         
         
-        if wordList:
-            return len(wordList[len(wordList)-1])
+        if wordList: # If s input was empty, this statement will be false and program will return 0
+            return len(wordList[len(wordList)-1]) # If the wordList variable contains 1=< characters, return the length of the last (length-1) word
         else:
-            return 0
-            
-            
-            # todo ## comment the code 
+            return 0 # Return 0 if wordList is empty
