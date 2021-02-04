e08b904931b74284c76bf5b98277cd2b912cf41b
Maciej <macko939@gmail.com>
2021-02-04 17:27:00 +0000

    Update plus_one.py
    Removed unnecessary variables in the calculations

--- solutions/plus_one.py
+++ solutions/plus_one.py
@@ -9,11 +9,11 @@ class Solution(object):
         init = len(digits) # Save the length of the original array for further use
         
         for i in range(len(digits)): # Iterate through the initial array and convert it to an integer
-            multiplier = 10 ** (len(digits) -1 - i) # Generate multiplier for each iteration of the loop, so that if i=0 we work on units, i=1 10's etc.
-            total = digits[i] * multiplier + total  # Add the (digit * multiplier) to the current total
+            
+            # Generate multiplier for each iteration of the loop, so that if i=0 we work on units, i=1 10's etc.
+            total = digits[i] * 10 ** (len(digits) -1 - i) + total  # Add the (digit * multiplier) to the current total
         
-        total += 1 # Increment total by 1 as per requirement 
-        total = map(int, str(total)) # generate a new array based on the total
+        total = map(int, str(total+1)) # generate a new array based on the total and increment total by 1 as per requirement 
         
          # While loop to pad out the array with zeros, in case the inital array comes with 0 at the beginning that will be lost
             # during the integer assembly loop
@@ -21,7 +21,3 @@ class Solution(object):
             total.insert(0, 0) 
          
         return total
-    
-    
-    # Todo
-    # Remove unneccesary variables such as multiplier
