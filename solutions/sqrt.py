50a9040fae6a6939dcb118685e164a4f21b9c4d1
Maciej <macko939@gmail.com>
2021-02-04 17:51:22 +0000

    Create sqrt.py

---
+++ solutions/sqrt.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        
+        result = x ** 0.5  
+        return int(result)
