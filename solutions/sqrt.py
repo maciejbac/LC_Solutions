79a9781353837b1fcdec14e77719620554efb7a4
Maciej <macko939@gmail.com>
2021-02-06 16:55:30 +0000

    Update sqrt.py

--- solutions/sqrt.py
+++ solutions/sqrt.py
@@ -5,11 +5,8 @@ class Solution(object):
         :rtype: int
         """
         
-        result = x ** 0.5  
-        return int(result)
+        result = x ** 0.5 # Calculate the x to the power of 0.5
+        return int(result) # Cast the result as int to truncate decimal places and return it
 
     
     
-    
-    
-    # todo: comment
