303b13a935a0cbaf16928e36992364cb0c038748
Maciej <macko939@gmail.com>
2021-02-09 16:35:56 +0000

    Update maximum_subarray.py

--- solutions/maximum_subarray.py
+++ solutions/maximum_subarray.py
@@ -1,3 +1,6 @@
+# Solution derived from Kadane's algorithm. Explanation found here: 
+# https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+
 class Solution(object):
     def maxSubArray(self, nums):
         """
