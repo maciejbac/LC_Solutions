b7fbde782e71268b1e6f567bec0488437d5690e4
Maciej <macko939@gmail.com>
2021-02-09 17:05:04 +0000

    Update staircase_problem.py

--- solutions/staircase_problem.py
+++ solutions/staircase_problem.py
@@ -1,5 +1,18 @@
 # WIP - NOT FINISHED, TOO SLOW FOR LEETCODE AND TIMES OUT
 
+# Resources:
+"""
+Remember the formula: ways(n) = ways(n-1) + ways(n-2)
+ways(4) = ways(3) + ways(2)
+ways(3) = ways(2) + ways(1)
+ways(2) = ways(1) + ways(0)
+ways(1) = 1
+ways(0) = 1
+This is the iterative Fibonacci Solution!
+
+https://quanticdev.com/algorithms/dynamic-programming/staircase-problems/
+"""
+
 class Solution(object):
     def climbStairs(self, n):
         """
