e1c1f468120a2c8e01a65c8b36aa49a8eac61329
Maciej <macko939@gmail.com>
2021-02-09 16:43:51 +0000

    Update maximum_subarray.py

--- solutions/maximum_subarray.py
+++ solutions/maximum_subarray.py
@@ -7,18 +7,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """        
-        maxFound = -100000
-        maxHere = 0
         
-        for i in range(len(nums)):
-            maxHere = maxHere + nums[i]
+        # Define initial variables 
+        maxFound = -100000 # maxFound set to the lowest number specified in the requirements
+        maxHere = 0 # Local max to store maximum sum subarray length 
+        
+        for i in range(len(nums)): # Iterate through all the numbers in the array
+            maxHere = maxHere + nums[i] # Add the nums[i] number to the existing highest sum 
 
-            if maxFound < maxHere:
+            if maxFound < maxHere: # Compare highest found sum to the current sum to determine if the current maxHere is higher
                 maxFound = maxHere
                 
+            # If maxHere is negative, set it to 0 to indicate that the highest sum array would be 0 (no numbers in array)
             if maxHere < 0:
                 maxHere = 0
-                
+        
+        # Return the highest sum subarray length
         return maxFound
             
             
