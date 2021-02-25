1f79bfb25e0942c401be94990474818b1d136009
Maciej <macko939@gmail.com>
2021-02-25 16:22:48 +0000

    Update staircase_problem_iterative.py

--- solutions/staircase_problem_iterative.py
+++ solutions/staircase_problem_iterative.py
@@ -1,3 +1,5 @@
+# Recursive method can be found here: https://github.com/macko939/LC_Solutions/blob/main/solutions/staircase_problem.py
+
 class Solution(object):
     def climbStairs(self, n):
         """
@@ -5,12 +7,13 @@ class Solution(object):
         :rtype: int
         """        
         
-        a, b = 1, 1
+        a, b = 1, 1 # Initialise first two steps in the fibonacci sequence
             
-        for node in range(n-1):
-            next_b = a + b
-            a = b
-            b = next_b
+        for node in range(n-1): # Use the range() function to determine how many steps of the fibonnacci sequence we need to calculate. 1 is taken away to avoid off-by-one errors.
+            nextB = a + b       # Calculate the value of the next step of the fibbonaci sequence.
+            
+            a = b               # Rearrange variables to allow the calculation of the next step in the fibonacci sequence               
+            b = nextB           #
                 
         return b
         
