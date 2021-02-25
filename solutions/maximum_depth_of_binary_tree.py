687b5880e145d08748a9a59f240997f85ba1b8b1
Maciej <macko939@gmail.com>
2021-02-25 16:35:35 +0000

    Update maximum_depth_of_binary_tree.py

--- solutions/maximum_depth_of_binary_tree.py
+++ solutions/maximum_depth_of_binary_tree.py
@@ -12,26 +12,26 @@ class Solution(object):
         """
         
         
-        def maxDepth(node):
+        def maxDepth(node): # Exit condition, return -1 if this is the bottom of the tree
             if node is None:
                 return -1 ; 
  
-            else:
+            else: # If not at the bottom, attempt calculating the depth of the tree where the current node is the root
                 lDepth = maxDepth(node.left)
                 rDepth = maxDepth(node.right)
  
-                if (lDepth > rDepth):
-                    return lDepth+1
+                if (lDepth > rDepth): # Tally up the depth of each branch of the tree steming from the current root node
+                    return lDepth + 1
                 else:
-                    return rDepth+1
+                    return rDepth + 1
  
-        root = TreeNode(1)
-        root.left = TreeNode(2)
-        root.right = TreeNode(3)
+        root = TreeNode(1) # Save a reference to the original root node
+        root.left = TreeNode(2)     # Assign left and right nodes to the corresponding variables for easier identification
+        root.right = TreeNode(3)    # 
         root.left.left = TreeNode(4)
         root.left.right = TreeNode(5)
         
-        return maxDepth(root)
+        return maxDepth(root)   # Recursively call the function until all branches of the binary tree were explored up to 'null' values
     
             
         """
