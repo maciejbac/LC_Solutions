ed91bee2eb915d305bbebb0237b340c297958e39
Maciej <macko939@gmail.com>
2021-09-06 11:11:24 +0100

    Delete solutions directory

--- solutions/remove_element.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        while val in nums: # Remove elements of value val as long as they exist in the List nums
-            nums.remove(val)
